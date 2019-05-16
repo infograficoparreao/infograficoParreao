5f30c7f1ec8b76a525ead05293b573a48315032e
Francisco <17554476+fcoalcantarajr@users.noreply.github.com>
2019-05-16 16:49:21 -0300

    Adicionada função para transição.
    Que ainda não funciona direito, mas vai funcionar. E permite que as telas respirem entre si.

--- aplication/draw.js
+++ aplication/draw.js
@@ -14,11 +14,12 @@ var EstadoTelaMapa = 3;
 var EstadoTelaContato = 4;
 var EstadoTelaPreservacao = 5;
 
+var alpha = 255;
 
 function draw(){
 	background(240);
-	cartaoAnimal.exibirPreserv();
-//	maquinaEstadoTelas();
+//	cartaoAnimal.exibirPreserv();
+	maquinaEstadoTelas();
 }
 
 function maquinaEstadoTelas(){
@@ -26,8 +27,12 @@ function maquinaEstadoTelas(){
 		case EstadoSplashs:
 			if(tempoSplash <= 180){
 				telaSpash.exibir();
-			} else if(tempoSplash > 180 && tempoSplash <= 360){
+			} else if(tempoSplash > 180 && tempoSplash <= 225){
+				transicao(alpha);
+			} else if(tempoSplash > 225 && tempoSplash <= 405) {
 				telaSpash2.exibir();
+			} else if(tempoSplash > 405 && tempoSplash <= 450) {
+				transicao(alpha);
 			}
 			else {
 				TELA = EstadoTelaInicial;
@@ -50,4 +55,13 @@ function maquinaEstadoTelas(){
 			telaConstrucao.exibir();
 			break;
 	}
+}
+
+function transicao() {
+	if(alpha>=1) {
+		fill(240, alpha);
+		rect(0, 0, width, height);
+		alpha -= 1;
+	}
+
 }
