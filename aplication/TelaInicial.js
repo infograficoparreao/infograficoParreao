15789fb95ed20354f462ad3af0a431b0858e4f04
bkellym <bkelly.sm@gmail.com>
2019-05-17 00:14:48 -0300

    Remodelagem Tela Inicial
    Detalhes: Inicio da Remodelagem da tela inicial para melhor arquitetura da informação

--- aplication/TelaInicial.js
+++ aplication/TelaInicial.js
@@ -1,39 +1,30 @@
 class TelaInicial{
 
-	constructor(imagemMapa, imagemContato, imagemPassaro, imagemGMaps, imagemPreserv){
-		this.diam = width/8;
+	constructor(imagemContato, imagemPassaro, imagemGMaps){
+		this.diam = width/12;
 		this.posX = width/4;
 		this.posY = height/4;
 
-		this.botaoMapa = new Objetos(imagemMapa, 2 * this.posX, 2 * this.posY, this.diam, this.diam);
-		this.botaoContato = new Objetos(imagemContato, 2 * this.posX, 2 * this.posY - (this.diam + this.diam/4), this.diam, this.diam);
-		this.botaoPassaro = new Objetos(imagemPassaro, 2 * this.posX - (this.diam + this.diam/4), 2 * this.posY, this.diam, this.diam);
-		this.botaoGMaps = new Objetos(imagemGMaps, 2 * this.posX, 2 * this.posY + (this.diam + this.diam/4), this.diam, this.diam);
-		this.botaoPreserv = new Objetos(imagemPreserv, 2 * this.posX + (this.diam + this.diam/4), 2 * this.posY, this.diam, this.diam);
+		this.botaoPassaro = new Objetos(imagemPassaro,  this.diam, 2 * this.posY - (this.diam + this.diam/4), this.diam, this.diam);
+		this.botaoContato = new Objetos(imagemContato, this.diam,  2 * this.posY, this.diam, this.diam);
+		this.botaoGMaps = new Objetos(imagemGMaps,  this.diam, 2 * this.posY + (this.diam + this.diam/4), this.diam, this.diam);
 	}
 
 	exibir(){
-		this.botaoMapa.interacaoClicarMudarTela(EstadoTelaMapa);
-		this.botaoMapa.interacaoHoverEllipse();
-		this.botaoMapa.exibir();
-
-		this.botaoContato.interacaoClicarMudarTela(EstadoTelaContato);
-		this.botaoContato.interacaoHoverEllipse();
-		this.botaoContato.exibir();
 
 		this.botaoPassaro.interacaoClicarMudarTela(EstadoTelaBiodiversidade);
 		this.botaoPassaro.interacaoHoverEllipse();
 		this.botaoPassaro.exibir();
 
+		this.botaoContato.interacaoClicarMudarTela(EstadoTelaContato);
+		this.botaoContato.interacaoHoverEllipse();
+		this.botaoContato.exibir();
+
 		this.botaoGMaps.interacaoClicarMaps();
 		this.botaoGMaps.interacaoHoverEllipse();
 		this.botaoGMaps.exibir();
 
-		this.botaoPreserv.interacaoClicarMudarTela(EstadoTelaPreservacao);
-		this.botaoPreserv.interacaoHoverEllipse();
-		this.botaoPreserv.exibir();
-
-		if(this.botaoMapa.hover || this.botaoContato.hover || this.botaoPassaro.hover || this.botaoGMaps.hover || this.botaoPreserv.hover){
+		if(this.botaoContato.hover || this.botaoPassaro.hover || this.botaoGMaps.hover){
 			cursor(HAND);
 		}
 		else{
